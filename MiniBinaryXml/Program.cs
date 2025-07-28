ca89342c5c936d61ce70c1674388a818f3f558b1
LX.. <1659404657@qq.com>
2025-07-28 14:44:11 +0800

    加入解压缩功能

--- MiniBinaryXml/Program.cs
+++ MiniBinaryXml/Program.cs
@@ -5,17 +5,39 @@ namespace MiniBinaryXml;
 class Program {
     static void Main(string[] args) {
         try {
-            string fromPath = args[0];
-            string toPath = args[1];
-            XElement xElement = XElement.Load(fromPath);
-            if (File.Exists(toPath)) File.Delete(toPath);
-            using (FileStream fileStream = new FileStream(toPath, FileMode.OpenOrCreate)) {
-                new MiniBinaryXmlWriter().Write(xElement, fileStream);
+            string mode = args[0];
+            string fromPath = args[1];
+            string toPath = args[2];
+            switch (mode) {
+                case "-c":
+                    Compress(fromPath, toPath);
+                    break;
+                case "-d":
+                    Decompress(fromPath, toPath);
+                    break;
             }
+
         }
         catch (Exception e) {
             Console.WriteLine(e);
             throw;
         }
+    }
+
+    static void Compress(string fromPath, string toPath) {
+        XElement xElement = XElement.Load(fromPath);
+        if (File.Exists(toPath)) File.Delete(toPath);
+        using (FileStream fileStream = new FileStream(toPath, FileMode.OpenOrCreate)) {
+            new MiniBinaryXmlWriter().Write(xElement, fileStream);
+        }
+    }
+
+    static void Decompress(string fromPath, string toPath) {
+        XElement xElement;
+        if (File.Exists(toPath)) File.Delete(toPath);
+        using (FileStream fileStream = new FileStream(toPath, FileMode.OpenOrCreate)) {
+            xElement = new MiniBinaryXmlReader().ReadAsXml(fileStream);
+        }
+        xElement.Save(toPath);
     }
 }
